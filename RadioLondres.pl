4c16603b39aee9315c7c5a7fa56ae22b642a6089
Freddy Frouin <freddy@linuxtribe.fr>
2019-09-24 17:41:23 +0200

    Update RadioLondres.pl

--- RadioLondres.pl
+++ RadioLondres.pl
@@ -1,2 +1,29 @@
-#!/usr/bin/perl -w
+#!/usr/bin/perl
+
+sub usage {
+	print "Usage: $0 {init|update} <path>\n";
+	exit 1;
+}
+
+usage unless (defined($ARGV[0]));
+usage unless ($ARGV[0] =~ /^(init|update$)/);
+usage unless (defined($ARGV[1]));
+usage unless (-d "$ARGV[1]");
+
+my $mode = $ARGV[0];
+my $dest = $ARGV[1];
+
+my @sources = `wget -O - https://raw.githubusercontent.com/ffrouin/radio-londres-2.0/master/youtube-sources.txt`;
+
+my ($cat,$ytref);
+foreach (@sources) {
+	if (/^([^\/]+)\/([\w\-]{11})$/) {
+		($cat,$ytref) = ($1, $2);
+		if ($mode eq 'init') {
+			system("mkdir \"$dest/$cat\"") unless (-d "$dest/$cat");
+			system("cd \"$dest/$cat\" && /usr/local/bin/youtube-dl -x --audio-format vorbis https://www.youtube.com/watch?v=$ytref --verbose");
+		} elsif ($mode eq 'update') {
+		}
+	}
+}
 
