4a84b127d817ea308023a0d66e3f80e26af8a165
ffrouin <freddy@linuxtribe.fr>
2019-10-14 14:42:00 +0200

    update

--- RadioLondres.sh
+++ RadioLondres.sh
@@ -1,48 +1,73 @@
 #!/bin/bash
+#
+# Script :	RadioLondres.sh
+# Object :	Used to manage "Radio Londres 2.0" playlist
+# Author :	Freddy Frouin <freddy@linuxtribe.fr>
+#
+# Date	 :	Mon Oct 14 14:22:33 CEST 2019
+# Release:	0.1
+#
+# ChangeLog:	0.1 init
+#
+################################################################################
+
+export ICECAST_PWD=YOUR_PASSWORD
 
 export MPD=/usr/local/bin/mpd
 export MPC=/usr/local/bin/mpc
 export TMP=/var/lib/mpd/RadioLondres/tmp
+export LOG=/var/lib/mpd/RadioLondres/log
 
 function usage {
 	echo "Usage:	$0 {bigben|radioLondres|gj|vp|media|politique|humour} {add|del}" 
-	echo "	$0 {health|publish-current}"
+	echo "	$0 {health|publish-current|log}"
 	echo
 	echo "	$0 tools del <song_name>"
-	echo "	$0 tools {check-playlist|init}"
+	echo "	$0 tools {check-playlist|reload|init|yt-sources|tt-sources}"
 	exit 1;
 }
 
+function log {
+	echo "[$(date)]	$1 $2" >>$LOG/$(date +%Y%m%d)_radioLondres.log
+}
+
 function insert {
+	log insert $1
 	$MPC insert $1
 }
 
 function insert_rand {
-	
+	log insert_rand $1
 	if [ -z "$2" ]; then
 		$MPC ls $1 |grep -vf $TMP/last.$1| shuf -n 1 > $TMP/last.$1
 	else
 		$MPC ls $1 |grep -vf $TMP/last.$1| shuf -n $2 > $TMP/last.$1
 	fi
 	cat $TMP/last.$1 |$MPC insert
+	while read s; do
+		log insert_rand $s
+	done <$TMP/last.$1
 }
 
 function delete {
+	log delete $1
 	$MPC playlist > $TMP/RadioLondres.delete.playlist
 	grep $1 -nr $TMP/RadioLondres.delete.playlist |awk -F: '{print $1}' |$MPC del
 }
 
 function delete_rand {
+	log delete_rand $1
 	while read s; do
 		$MPC playlist > $TMP/RadioLondres.delete_rand.playlist
 		grep "$s" -nr $TMP/RadioLondres.delete_rand.playlist |awk -F: '{print $1}' |$MPC del
+		log delete_rand $s
 	done <$TMP/last.$1
 }
 
 case "$1" in
 publish-current)
 	$MPC current > ~/RadioLondres/RadioLondres.current
-	scp ~/RadioLondres/RadioLondres.current ns5.linuxtribe.fr:/tmp/ 
+	scp ~/RadioLondres/RadioLondres.current YOUR_HOST:/tmp/ 
 	;;
 health)
 	p=$(ps -edf | grep -v grep |grep -c mpd.conf)
@@ -56,6 +81,12 @@ health)
 	*)
 		;;
 	esac
+	;;
+log)
+	export CLIENTS=$(wget --user=admin --password=$ICECAST_PWD http://localhost:8000/admin/listmounts.xsl --quiet -O - |grep "Listener(s)" | head -1 | perl -pe 's/^.*(\d+).*$/$1/')
+	export CLIENTS_LOW=$(wget --user=admin --password=$ICECAST_PWD http://localhost:8000/admin/listmounts.xsl --quiet -O - |grep "Listener(s)" | tail -1 | perl -pe 's/^.*(\d+).*$/$1/')
+
+	echo "[$(date)]	[HQ:$CLIENTS LOW:$CLIENTS_LOW] $($MPC status | head -1)" >>$LOG/$(date +%Y%m%d)_radioLondres.log
 	;;
 bigben)
 	case "$2" in
@@ -97,10 +128,14 @@ vp)
         add)
 		insert jingle/$1_footer.ogg
 		insert_rand $1
+		insert jingle/Notre_President_sur_les_violences_policieres_-_L_etat_de_droit-fcWZSfG0aCs.ogg
+		insert jingle/$1_header1.ogg
 		insert jingle/$1_header.ogg
                 ;;
         del)
 		delete jingle/$1_header.ogg
+		delete jingle/$1_header1.ogg
+		delete jingle/Notre_President_sur_les_violences_policieres_-_L_etat_de_droit-fcWZSfG0aCs.ogg
 		delete_rand $1
 		delete jingle/$1_footer.ogg
                 ;;
@@ -144,8 +179,18 @@ tools)
 	check-playlist)
 		/usr/local/bin/mpc playlist |grep -v chanson|grep -v accordeon|grep -v jazz
 		;;
+	reload)
+		$MPC update
+		$MPC crop
+		$MPC ls chanson| $MPC add
+                $MPC ls jazz | $MPC add
+                $MPC ls accordeon | $MPC add
+                for i in `seq 1 100`; do
+                        $MPC shuffle
+                done
+		;;
 	init)
-		$MPC stop
+		$MPC update
 		$MPC clear
 		$MPC ls chanson| $MPC add
 		$MPC ls jazz | $MPC add
@@ -154,6 +199,12 @@ tools)
 			$MPC shuffle
 		done
 		$MPC play
+		;;
+	yt-sources)
+		cd /var/lib/mpd/music && find . -type f |grep ogg$ | perl -pe 's/\.\/([^\/]+)\/.+\-([\w\d\_\-]{11})[\_\-]?\d?\d?\.ogg$/$1\/$2/' |grep -v ^\.\/ | uniq -u
+		;;
+	tt-sources)
+		cd /var/lib/mpd/music && find . -type f |grep ogg$ | perl -pe 's/\.\/([^\/]+)\/.+\-(\d{19})[\_\-]?\d?\d?\.ogg$/$1\/$2/' |grep -v ^\.\/ | uniq -u
 		;;
 	*)
 		usage
